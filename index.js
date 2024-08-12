a221c17647e5514ef18347ab88880f797eca12e4
njanfang <njanfang@gmail.com>
2024-08-12 16:29:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
             <title>Welcome to Cloud njanfang Youtube channel ,Please share & subscribe , Thank you </title>
         </head>
         <body>
-            <h1>Welcome to Cloud njanfang Youtube channel ,Please share & subscribe , Thank you very much for 60 subscribers , keep loving , thank you </h1>
+            <h1>Welcome to Cloud njanfang Youtube channel ,Please share & subscribe , Thank you very much for 6 subscribers , keep loving , thank you </h1>
             <p>Don't forget to subscribe & share with friends</p>
         </body>
         </html>
